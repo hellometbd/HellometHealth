daeee0e96ef9b625a19ebfe929689595a79fdaed
hellometbd <77330755+hellometbd@users.noreply.github.com>
2021-01-21 17:40:51 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,119 +1,65 @@
 const express = require('express');
 
 const app = express();
+const { ifError } = require("assert");
 const { json } = require("body-parser");
 app.use(express.json());
 var google = require('google')
+var storage, path;
+path = require('path');
+const multer = require("multer");
 
-const addMedicine = require('./addMedicine');
+
+
+const addMedicine = require('./medicine');
+const pharmacy = require('./pharmacy');
+
+
 
 app.use("/medicine", addMedicine);
+app.use("/pharmacy",pharmacy);
 
-var MongoClient = require('mongodb').MongoClient;
-var config = { useUnifiedTopology: true };
-var connectionUrl = "mongodb+srv://hellomethealth:hellomethealth@cluster0.vrnxz.mongodb.net?retryWrites=true&w=majority";
 
-app.post('/addPharmacy', function(req, res){
-
-    MongoClient.connect(connectionUrl,config, function(error, Client){
-        if(error){
-            console.log(error);
-            res.json({ message: "Connection Failed" });
-            res.end();
-        }else{
-            let pharmacy_meta_data = req.body;
-            console.log("Connection Success.");
-            console.log(pharmacy_meta_data);
-            
-            let db = Client.db("HellometHealth");
-            let collec = db.collection("pharmacy");
-
-            let pharmacyId = "P" + Date.now();
-            // set id for every part of pharmacy data...
-            let pharmacy_data = {
-                _id: pharmacyId,
-                meta_data: pharmacy_meta_data.meta_data
-            }
-
-        collec.insertOne(pharmacy_data, function(error, result){
-            if(error){
-                console.log("uploading pharmacy meta_data to MongoDB has Failed: error: " + error);
-                res.json({ message: "Meta-data upload Failed" });
-                res.end();
-            }else{
-                console.log("uploading pharmacy meta_data to MongoDB has successful.");
-                res.json({ message: "Pharmacy successfully added." })
-                console.log(result);
-            }
-        })           
-        }
-    });
+storage = multer.diskStorage({
+    destination: './Images/',
+    filename: function (req, file, cb) {
+        return cb(null, "image_" + new Date().getTime() + (path.extname(file.originalname)));
+    }
 });
 
-app.get('/getPharmacies', function(req, res){
+var upload = multer({ storage: storage })
 
-    MongoClient.connect(connectionUrl, config, function(error, Client){
-        if(error){
-            console.log(error);
-        }else{
-            let db = Client.db("HellometHealth");
-            let collec = db.collection("pharmacy");
-            var query = {};
-            collec.find(query).toArray(function(error, result){
-                if (error) {
-                    console.log(error);
-                }else{
-                    res.json(result);
-                    res.end();
-                }
-            })
-        }
-    })
-
-})
+// uploading image to server hadrdisk then reference will insert to MongoDb...
+app.post("/uploadImageToGenarateUrl",
+    multer({
+        storage: storage
+    }).single('uploadImage'),
+    function (req, res) {
+        console.log(__dirname + "/Images/" + req.file.filename);
+        var ref = { url: "hellometbd.com/Images/" + req.file.filename }
+        res.json(ref);
+    }
+);
 
 
-app.post('/getAddressFromLatLng',function(req,res){
-    const geocoder = new google.maps.Geocoder();
 
-
-        let latitude = req.body.latitude;
-        let longitude = req.body.longitude;
-        const latlng = {
-            lat: parseFloat(latitude),
-            lng: parseFloat(longitude),
-          };
-
-          geocoder.geocode({ location: latlng }, (results, status) => {
-            if (status === "OK") {
-              if (results[0]) {
-               
-               console.log(results[0].formatted_address);
-              } else {
-                window.alert("No results found");
-              }
-            } else {
-              window.alert("Geocoder failed due to: " + status);
-            }
-          });
-    
-
+ app.get("/Images/:imageName", (req, res) => {
+    file = req.params.imageName;
+  res.sendFile(path.join(__dirname, "./Images/"+file));
 });
 
-app.get('/get',function(req, res){
-    res.writeHead(200, {'Content-Type':'text/plain'});
-    res.end("Success");
-})
+
+//For Cpanel...
+//app.listen(3000);
 
 
+//For Cloud Hosting...
 const PORT = process.env.PORT || 1010;
-
 app.listen(PORT, function(error){
     if(error){
         console.log(error);
     }else{
         console.log('Server is running on port :' + PORT);
     }
-
 });
 
