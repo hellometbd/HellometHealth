600d0e42ac5399d240340921b51148b8b08c4efe
kh1amirhamza <kh7amirhamza@gmail.com>
2021-03-10 13:52:31 +0600

    new Commit

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use("/deliveryman", deliveryman);
 app.use("/practice", practice);
 
 app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.append('Access-Control-Allow-Headers', 'Content-Type');
     next();
