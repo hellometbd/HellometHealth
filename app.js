3671e7c3a7942a3d8172f1900b1eda1a5335a5e6
kh1amirhamza <kh7amirhamza@gmail.com>
2021-03-10 14:13:19 +0600

    new

--- app.js
+++ app.js
@@ -26,6 +26,8 @@ app.use((req, res, next) => {
     res.append('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.append('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+res.append('Access-Control-Allow-Credentials', 'true');
+
     next();
 });
 
